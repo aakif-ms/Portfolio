9daf541ea4c16b361ad69b6e4a1c834abe3a624d
aakif-ms <aakif.msiddiqui@gmail.com>
2026-04-10 11:51:58 +0530

    update technologies section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ export default function Hero() {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
             >
-              Full Stack Developer
+              Full-Stack AI Engineer
             </motion.span>
             <motion.p
               variants={container(1)}
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,10 +5,16 @@ import TechComponent from "./TechComponent";
 import { RiReactjsLine } from "react-icons/ri";
 import { TbBrandNextjs } from "react-icons/tb";
 import mongoDb from "../assets/mongodb.svg";
+import python from "../assets/python.svg";
+import nextjs from "../assets/nextjs.svg";
+import js from "../assets/javascript.svg";
 import { FaNodeJs } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { SiPytorch } from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
+import { SiLangchain } from "react-icons/si";
+import { SiLanggraph } from "react-icons/si";
+
 import cpp from "../assets/cpp.svg";
 
 export default function Technologies() {
@@ -28,13 +34,17 @@ export default function Technologies() {
         initial={{ x: -100, opacity: 0 }}
         transition={{ duration: 1.5 }}
         className="flex flex-wrap items-center justify-center gap-4">
+        <TechComponent name="Python" src={python} classes="w-[70px]" />
+        <TechComponent name="C++" src={cpp} classes="w-16" />
+        <TechComponent name="Javascript" src={js} classes="w-16" />
         <TechComponent name="React" icon={RiReactjsLine} classes="text-7xl text-cyan-400" />
-        <TechComponent name="NextJS" icon={TbBrandNextjs} classes="text-7xl" />
+        <TechComponent name="NextJS" src={nextjs} classes="w-16" />
         <TechComponent name="NodeJS" icon={FaNodeJs} classes="text-7xl text-green-600" />
         <TechComponent name="ExpressJS" icon={SiExpress} classes="text-7xl " />
         <TechComponent name="PyTorch" icon={SiPytorch} classes="text-7xl text-orange-600" />
+        <TechComponent name="LangChain" icon={SiLangchain} classes="text-7xl text-emerald-800"/>
+        <TechComponent name="LangGraph" icon={SiLanggraph} classes="text-7xl text-emerald-800"/>
         <TechComponent name="MongoDB" src={mongoDb} classes="w-[70px]" />
-        <TechComponent name="C++" src={cpp} classes="w-16" />
         <TechComponent name="PostgreSQL" icon={BiLogoPostgresql} classes="text-7xl text-[#396c94] bg-white rounded-full" />
       </motion.div>
     </div>
