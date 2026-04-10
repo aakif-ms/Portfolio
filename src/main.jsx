ed3f5650ab27df47a9331cdf6858d491a3013447
aakif-ms <aakif.msiddiqui@gmail.com>
2026-04-10 12:16:46 +0530

    add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import portfolioFavicon from './assets/portfolio.png'
+
+const faviconEl = document.querySelector("link[rel='icon']") || document.createElement('link')
+faviconEl.rel = 'icon'
+faviconEl.type = 'image/png'
+faviconEl.href = portfolioFavicon
+document.head.appendChild(faviconEl)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
